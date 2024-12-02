efc0c3964ce208018064684ba12df0e61d297018
typesafety <ailoodee@gmail.com>
2024-12-02 01:12:52 +0100

    Add solution for day 1 part 2

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 pub fn solve_part1(input: &str) -> String {
-    let (mut left, mut right) = parse_lists(input);
+    let (mut left, mut right) = parse_input(input);
     left.sort();
     right.sort();
 
@@ -11,20 +13,29 @@ pub fn solve_part1(input: &str) -> String {
     format!("{total_distance}")
 }
 
-pub fn solve_part2(_input: &str) -> String {
-    unimplemented!()
+pub fn solve_part2(input: &str) -> String {
+    let (left, right) = parse_input(input);
+
+    let mut occurrences = HashMap::<u32, u32>::new();
+    for num in right {
+        occurrences.entry(num).and_modify(|n| *n += 1).or_insert(1);
+    }
+    format!(
+        "{}",
+        left.into_iter()
+            .fold(0, |acc, n| acc + n * occurrences.get(&n).unwrap_or(&0))
+    )
 }
 
-fn parse_lists(input: &str) -> (Vec<u32>, Vec<u32>) {
-    parse_pairs(input).into_iter().unzip()
-}
-
-fn parse_pairs(input: &str) -> Vec<(u32, u32)> {
+fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
     input
         .split("\n")
         .map(|s| {
             let split: Vec<&str> = s.split_whitespace().collect();
-            (split[0].parse().unwrap(), split[1].parse().unwrap())
+            (
+                split[0].parse::<u32>().unwrap(),
+                split[1].parse::<u32>().unwrap(),
+            )
         })
-        .collect()
+        .unzip()
 }
