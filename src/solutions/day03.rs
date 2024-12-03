ee8734267c62c833a581cfa3f946be4ef9782f92
typesafety <ailoodee@gmail.com>
2024-12-03 21:05:43 +0100

    Add result for day 3 part 1

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -1,7 +1,34 @@
-pub fn solve_part1(_input: &str) -> String {
-    unimplemented!()
+use regex;
+
+pub fn solve_part1(input: &str) -> String {
+    let pattern = regex::Regex::new(r"mul\((?<multiplier>\d+),(?<multiplicant>\d+)\)").unwrap();
+    let muls = pattern.captures_iter(input).map(Mul::from_captures);
+    let result: u32 = muls.map(|mul| mul.eval()).sum();
+
+    format!("{result}")
 }
 
 pub fn solve_part2(_input: &str) -> String {
     unimplemented!()
+}
+
+#[derive(Debug)]
+struct Mul {
+    multiplier: u32,
+    multiplicant: u32,
+}
+
+impl Mul {
+    fn from_captures(cap: regex::Captures) -> Mul {
+        let multiplier = cap["multiplier"].parse().unwrap();
+        let multiplicant = cap["multiplicant"].parse().unwrap();
+        Mul {
+            multiplier,
+            multiplicant,
+        }
+    }
+
+    fn eval(&self) -> u32 {
+        self.multiplier * self.multiplicant
+    }
 }
