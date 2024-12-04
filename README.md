bbd709352094eeb648c1ef3e8b623195c8055b62
typesafety <ailoodee@gmail.com>
2024-12-04 23:52:30 +0100

    Add solution for day 4 part 2

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Solutions are located in
 * [Day 1](https://github.com/typesafety/aoc2024/tree/main/src/solutions/day01.rs)
 * [Day 2](https://github.com/typesafety/aoc2024/tree/main/src/solutions/day02.rs)
 * [Day 3](https://github.com/typesafety/aoc2024/tree/main/src/solutions/day03.rs)
+* [Day 4](https://github.com/typesafety/aoc2024/tree/main/src/solutions/day04.rs)
 
 ## Options
 
--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -13,8 +13,53 @@ pub fn solve_part1(input: &str) -> String {
     format!("{}", count)
 }
 
-pub fn solve_part2(_input: &str) -> String {
-    unimplemented!()
+pub fn solve_part2(input: &str) -> String {
+    let grid = parse_input(input);
+    let mut count = 0;
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, _) in row.iter().enumerate() {
+            let x = x.try_into().unwrap();
+            let y = y.try_into().unwrap();
+
+            let is_a = get(&(x, y), &grid) == Some('A');
+            if is_a && is_xmas((x, y), &grid) {
+                count += 1;
+            }
+        }
+    }
+
+    format!("{}", count)
+}
+
+fn is_xmas(point: Point, grid: &Grid) -> bool {
+    fn get_letters(middle: Point, dirs: impl Iterator<Item = Direction>, grid: &Grid) -> Vec<char> {
+        dirs.filter_map(|d| get(&next_point(middle, &d), grid))
+            .collect()
+    }
+
+    fn is_mas(chars: Vec<char>) -> bool {
+        let ms: Vec<char> = "MS".chars().collect();
+        let mut cs: Vec<char> = chars.clone();
+        cs.sort();
+        cs == ms
+    }
+
+    let d7_d3 = [Direction::D7, Direction::D3];
+    let d1_d9 = [Direction::D1, Direction::D9];
+
+    let d7_d3_is_mas = is_mas(get_letters(point, d7_d3.into_iter(), grid));
+    let d1_d9_is_mas = is_mas(get_letters(point, d1_d9.into_iter(), grid));
+
+    d7_d3_is_mas && d1_d9_is_mas
+}
+
+fn get(point: &Point, grid: &Grid) -> Option<char> {
+    if inside(*point, grid) {
+        Some(grid[get_y(point)][get_x(point)])
+    } else {
+        None
+    }
 }
 
 type Grid = Vec<Vec<char>>;
@@ -53,15 +98,15 @@ fn read_word(word: &str, grid: &Grid, start: Point) -> u32 {
         .unwrap()
 }
 
+fn get_x(p: &Point) -> usize {
+    p.0.try_into().unwrap()
+}
+
+fn get_y(p: &Point) -> usize {
+    p.1.try_into().unwrap()
+}
+
 fn try_read_word_dir(word: &str, grid: &Grid, start: Point, dir: &Direction) -> bool {
-    fn get_x(p: &Point) -> usize {
-        p.0.try_into().unwrap()
-    }
-
-    fn get_y(p: &Point) -> usize {
-        p.1.try_into().unwrap()
-    }
-
     let mut point = start;
 
     for (ix, c) in word.chars().enumerate() {
